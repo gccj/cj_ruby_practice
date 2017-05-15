cdecf446db3475ae955efaeb718390d3b7feddfd
Chen Jun <chen.jun@livesense.co.jp>
2017-05-15 11:05:22 +0900

    For PR

---
+++ json.rb
@@ -0,0 +1,20 @@
+hash_object = eval ARGV[0].gsub(/:/, '=>')
+
+def print_hash_array(hash_array)
+  hash_array.each do |hash|
+    hash.each do |key, value|
+      puts "  #{key}"
+      puts "    #{value}"
+    end
+  end
+end
+
+hash_object.each do |key, value|
+  if value.is_a? Array
+    puts "#{key}"
+    print_hash_array value
+  else
+    puts "#{key}"
+    puts "  #{value}"
+  end
+end
---
+++ kako.rb
@@ -0,0 +1,69 @@
+#「(」と「)」の数をチェック
+def number_equal?(text)
+  text.count('(') == text.count(')')
+end
+
+#最初の括弧は「(」であるかどうかをチェック
+def left_legal?(text)
+  text[0] == '('
+end
+
+#最後の括弧は「)」であるかどうかをチェック
+def right_legal?(text)
+  text[-1] == ')'
+end
+
+def space
+  ' '
+end
+
+def left?(char)
+  char == '('
+end
+
+def right?(char)
+  char == ')'
+end
+
+#括弧の対応関係をチェック
+def legal?(text)
+  !text.empty? && number_equal?(text) && left_legal?(text) && right_legal?(text)
+end
+
+#開始（　と閉じる　）の間にスペースの数 defaultは0
+def number_of_space(num = 0)
+  @number_of_space ||= num.to_i
+end
+
+#開始（　と閉じる　）の間にスペースを入れるかどうか
+def need_space?
+  number_of_space > 0
+end
+
+
+#使用：「ruby kako.rb '8(())9()()()(rehgwerh)(erhyewrhw)()   ()' 2」でも処理する。
+
+text = ARGV[0].gsub(/[^\(\)]/, '') #不正のアルファベット、数値とスペースなどを取り除く
+
+number_of_space ARGV[1] #第二引数は開始（　と閉じる　）の間にスペース数を受け取る
+
+#括弧の対応関係が不正な場合エラーメッセージを表示して停止する
+abort('括弧の対応関係が不正です。(()())のような正しいInputしてください') unless legal?(text)
+
+@prechar = text.slice! 0
+puts @prechar
+@offset = 0
+text.chars do |char|
+  set_space = 0
+  if left?(char) && left?(@prechar)
+    @offset += 2
+  elsif right?(char) && right?(@prechar)
+    @offset -= 2
+  elsif need_space? && right?(char) && left?(@prechar)
+    set_space = number_of_space
+  end
+  @offset = 0 if @offset < 0
+  puts "#{space * (@offset + set_space)}#{char}"
+  @prechar = char
+end
+ 
